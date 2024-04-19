7d76298c89af2450a77078d64e71fc29b2b7eaa4
Aizel Cantado <201920253@psu.palawan.edu.ph>
2024-04-19 13:04:17 +0800

    Views.py modified

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -6,13 +6,16 @@ from studentorg.models import Organization
 from studentorg. forms import OrganizationForm
 from django.urls import reverse_lazy
 
-template_name = "home.html"
-
-class OrganizationListView):
-    model = Organization
-    context_object_name = 'organization'
-    template_name = 'org_list.html'
     paginated_by = 5
+
+
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = 'OrganizationForm'
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('organization-list')
+    
 
 
 
